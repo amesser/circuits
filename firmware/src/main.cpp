4b3b9a74d4bdd6e80891c408716c22eab87d7f86
Andreas Messer <andi@bastelmap.de>
2013-10-01 19:39:43 +0200

    Prepared firmware directory structure

---
+++ firmware/src/main.cpp
@@ -0,0 +1,38 @@
+/*
+ *  Copyright 2013 Andreas Messer <andi@bastelmap.de>
+ *
+ *  This file is part of SDCard Data Logger firmware.
+ *
+ *  This software is free software: you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License as
+ *  published by the Free Software Foundation, either version 3 of
+ *  the License, or (at your option) any later version.
+ *
+ *  This software is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this software.  If not, see <http://www.gnu.org/licenses/>.
+ *  */
+#include <target/attiny2313.hpp>
+#include <generic/buffer.hpp>
+#include <devices/mcp3204.hpp>
+#include <devices/sdcard.hpp>
+#include <util/datatypes.hpp>
+
+using namespace Platform::Architecture::AVR8;
+using namespace Platform::Target::AVR8;
+using namespace Platform::Buffer;
+using namespace Platform::Devices;
+using namespace Platform::Util::Datatypes;
+
+
+int main(void) {
+  while(1);
+}
+
+
+
+
