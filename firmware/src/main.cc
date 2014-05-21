f59d39ff61b3655ff8f8d2cecf342e99882dfa6a
Andreas Messer <andi@bastelmap.de>
2014-05-21 14:43:04 +0200

    Initial directory structure

---
+++ firmware/src/main.cc
@@ -0,0 +1,18 @@
+/*
+ * main.cc
+ *
+ *  Created on: 20.05.2014
+ *      Author: andi
+ */
+
+#include <stdint.h>
+
+
+int main()
+{
+  volatile uint8_t fRun;
+
+  while(fRun == fRun);
+
+  return 0;
+}
