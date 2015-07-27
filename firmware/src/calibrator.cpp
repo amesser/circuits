5a0989ed9655379272c5a8e401ccd2bf240195df
Andreas Messer <andi@bastelmap.de>
2015-07-27 22:02:11 +0200

    Implement LCD Display for calibrator

--- firmware/src/calibrator.cpp
+++ firmware/src/calibrator.cpp
@@ -7,17 +7,21 @@
 
 #include "calibrator_bsp.hpp"
 
+static CalibratorBsp g_bsp;
+static const char abTest[] = "ABCDEFGH";
 
 int main(void) __attribute__ ((OS_main));
 int main(void)
 {
   CalibratorBsp::LCDType lcd;
 
+  g_bsp.init();
+
 
   while(1)
   {
-    lcd.init();
-
+    lcd.moveCursor(0);
+    lcd.writeData(abTest, 8);
   }
 }
 
--- firmware/src/calibrator_bsp.hpp
+++ firmware/src/calibrator_bsp.hpp
@@ -16,25 +16,50 @@ using namespace ecpp::Peripherals;
 
 class CalibratorBsp
 {
-private:
-  class LcdBsp {
-  private:
+protected:
     static IOPin<AVR_IO_PD1> PinE;
     static IOPin<AVR_IO_PD2> PinRw;
     static IOPin<AVR_IO_PD3> PinRs;
+
     static IOPort<AVR_IO_PC> PortNibble;
+    static IOPort<AVR_IO_PD> PortLcdControl;
+
+  class LcdBsp
+  {
+  protected:
+    static const FlashVariable<HD44780_CMD, 5> HD44780InitSequence PROGMEM;
+
+
   protected:
     static void delay(uint16_t us) { while(us--) _delay_us(1);}
-    static void clearRW()     {PinRw = 0;}
+    static void setRW()       {PortNibble.updateDirection(0x0F,0x00); PinRw = 1; }
+    static void clearRW()     {PinRw = 0; PortNibble.updateDirection(0x0F,0x0F);}
+    static void setRS()       {PinRs = 1;}
     static void clearRS()     {PinRs = 0;}
     static void clearEnable() {PinE  = 0;}
     static void setEnable()   {PinE  = 1;}
     static void setNibble(uint8_t data) {PortNibble.updateOutputs(data, 0x0F);}
   };
 public:
+
   typedef LCD_HD44780<HD44780_MODE_4BIT, LcdBsp> LCDType;
+
+
+  static void init()
+  {
+    PortLcdControl = PinE.OutHigh | PinRw.OutHigh | PinRs.OutHigh;
+    LCDType().init();
+  }
 };
 
+const FlashVariable<HD44780_CMD, 5> CalibratorBsp::LcdBsp::HD44780InitSequence PROGMEM =
+{
+   HD44780_CMD_FUNCTIONSET_4BIT | HD44780_CMD_FUNCTIONSET_2LINE | HD44780_CMD_FUNCTIONSET_5x7FONT,
+   HD44780_CMD_DISPLAYCONTROL_DISPLAYON,
+   HD44780_CMD_ENTRYMODE_INCREMENT | HD44780_CMD_ENTRYMODE_NOSHIFT,
+   HD44780_CMD_HOME,
+   HD44780_CMD_CLEAR,
+};
 
 
 
