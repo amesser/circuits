ad5e4036306a2445372d49f97c49af1c3196b9f9
Andreas Messer <andi@bastelmap.de>
2018-02-10 18:56:48 +0100

    Fix improper handling of read timeout

--- misc/lirc_rs232.py
+++ misc/lirc_rs232.py
@@ -1,26 +1,5 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# 
-# Copyright 2015 Andreas Messer <andi@bastelmap.de>
-# 
-# RS232 Infrared Receiver to Lirc UDP Converter
-# 
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General  *  Public License as published
-# by the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-# 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-# 
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-# Purpose of this little script is to convert the timing
-# information delivered by the receiver hardware
-# into a format that lirc can handle
 
 import serial
 import struct
@@ -39,20 +18,18 @@ port = int(port)
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect((host,port))
 
-sio = serial.Serial(args.device,38400,parity=serial.PARITY_EVEN)
-sio.timeout=5
+sio = serial.Serial(args.device,38400,parity=serial.PARITY_EVEN,
+                    timeout=0.3)
 
 data = ""
 while True:
   value = sio.read(3)
 
-  if value is None:
-    if len(data) < 3:
-      data = ""
-  else:
+  if value:
     data = data + value
+  else:
+    data = ""
   
-
   while len(data) >= 3:
     prefix, time_delta = struct.unpack_from(">BH", data, 0)
     data = data[3:]
@@ -66,11 +43,11 @@ while True:
       udp_data = 0x7FFF
 
     if prefix & 0x80:
-      # print "space %d" % time_delta
       udp_data = udp_data | 0x8000
+      print "space %d" % time_delta
     else:
-      #print "pulse %d" % time_delta
       pass
+      print "pulse %d" % time_delta
 
     try:
       s.send(struct.pack("<H", udp_data))
